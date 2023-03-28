96cd39c791f91387d0fd07ebe00f854c608856f3
kalaitzisxristos <kalaitzisxristos@hotmail.com>
2023-03-29 02:10:12 +0300

    replace files

--- lib/supabase/select_race_track_functions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_complete_guide/models/race.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-final supabase = Supabase.instance.client;
-Future<String> getRaceTrack({required RaceTrack race}) async {
-  final image =
-      supabase.storage.from('tracks').getPublicUrl(race.name + '.png');
-  return image;
-}
--- lib/widgets/desktop_widgets/panther_desktop_widget.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/names.dart';
-
-Widget PantherDesktop() {
-  return Center(child: Text(panther));
-}
